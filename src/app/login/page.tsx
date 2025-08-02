daa65c645984976deb3b7e8fe1551c7587dd62ec
portfolio-web <jmesparre@gmail.com>
2025-08-02 17:32:48 -0300

    se agrego login firbase2

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,11 +1,9 @@
-import { LoginForm } from "@/components/login-form"
-
 export default function Page() {
   return (
     <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm">
-        <LoginForm />
+        <p>Please use the login button in the header to sign in.</p>
       </div>
     </div>
-  )
+  );
 }
