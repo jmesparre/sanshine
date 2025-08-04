74bc0bea2ec5be0e109de0f0f97fb6f9934886f6
portfolio-web <jmesparre@gmail.com>
2025-08-04 20:59:00 -0300

    se agrego api mercadopago2

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,11 +1,13 @@
-import { LoginForm } from "@/components/login-form"
-
 export default function Page() {
   return (
     <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
-      <div className="w-full max-w-sm">
-        <LoginForm />
+      <div className="w-full max-w-sm text-center">
+        <h1 className="text-2xl font-bold">Iniciar Sesión</h1>
+        <p className="mt-4 text-gray-600">
+          Por favor, utiliza el botón de "Iniciar Sesión" en la parte superior
+          para acceder a tu cuenta.
+        </p>
       </div>
     </div>
-  )
+  );
 }
