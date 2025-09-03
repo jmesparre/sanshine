008df8df0053f027e5afaf8e4b68d254a64d06a9
portfolio-web <jmesparre@gmail.com>
2025-09-03 19:51:13 -0300

    se soluciono en error de el boton de paypal que no aparecia2

--- src/components/payments/PaypalPagoButton.tsx
+++ src/components/payments/PaypalPagoButton.tsx
@@ -20,7 +20,7 @@ const PaypalPagoButton: React.FC<PaypalPagoButtonProps> = ({ amount, currency, s
   const { user, promptLogin } = useAuth();
 
   const paypalOptions = {
-    clientId: process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID?.trim() || "test",
+    clientId: process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID?.replace(/\s/g, "") || "test",
     currency: currency,
     intent: "capture",
   };
